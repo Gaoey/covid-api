a2def98f4abf2bbf57ad8a1e1a3942a2043d0152
gaoey <gaoey@hotmail.com>
2021-06-14 10:30:09 +0700

    feat: add test for get summary

--- models/response.go
+++ models/response.go
@@ -3,16 +3,12 @@ package models
 type CovidStatResponse struct {
 	Data []CovidStat `json:"Data"`
 }
-
 type CovidStat struct {
-	Age        int    `json:"Age"`
-	Province   string `json:"Province"`
-	ProvinceId int    `json:"ProvinceId"`
+	Age      *int   `json:"Age"`
+	Province string `json:"Province"`
 }
 
-type Province map[string]int
-type AgeGroup map[string]int
 type CovidSummaryResponse struct {
-	Province Province
-	AgeGroup AgeGroup
+	Province map[string]int
+	AgeGroup map[string]int
 }
--- summary/summary_test.go
+++ summary/summary_test.go
@@ -8,12 +8,70 @@ import (
 	"github.com/Gaoey/covid-api/models"
 )
 
-func Test_UnmarshalResponse(t *testing.T) {
-	respString := "{ \"Data\": [ { \"ConfirmDate\": \"2021-05-04\", \"No\": null, \"Age\": 51, \"Gender\": \"หญิง\", \"GenderEn\": \"Female\", \"Nation\": null, \"NationEn\": \"China\", \"Province\": \"Phrae\", \"ProvinceId\": 46, \"District\": null, \"ProvinceEn\": \"Phrae\", \"StatQuarantine\": 5 } ]}"
+func Test_Unmarshal_CovidStatResponse(t *testing.T) {
+	respString := "{ \"Data\": [ { \"ConfirmDate\": \"2021-05-04\", \"No\": null, \"Age\": null, \"Gender\": \"หญิง\", \"GenderEn\": \"Female\", \"Nation\": null, \"NationEn\": \"China\", \"Province\": \"Phrae\", \"ProvinceId\": 46, \"District\": null, \"ProvinceEn\": \"Phrae\", \"StatQuarantine\": 5 } ]}"
 	var covidStatResponse models.CovidStatResponse
 	if err := json.Unmarshal([]byte(respString), &covidStatResponse); err != nil {
 		t.Fatalf("error: %v", err)
 	}
 
 	fmt.Printf("covidStatResponse: %v", covidStatResponse)
+}
+
+type TestCase struct {
+	given interface{}
+	want  interface{}
+}
+
+func Test_Covid_Summary(t *testing.T) {
+	// preparation
+	fiftyOne := new(int)
+	twenty := new(int)
+	*fiftyOne = 51
+	*twenty = 20
+	// testcase
+	testcases := []TestCase{
+		{
+			given: []models.CovidStat{
+				{
+					Age:      fiftyOne,
+					Province: "Phrae",
+				},
+				{
+					Age:      fiftyOne,
+					Province: "Udon Thani",
+				},
+				{
+					Age:      twenty,
+					Province: "Udon Thani",
+				},
+				{
+					Age:      nil,
+					Province: "Phrae",
+				},
+			},
+			want: models.CovidSummaryResponse{
+				Province: map[string]int{
+					"Phrae":      2,
+					"Udon Thani": 2,
+				},
+				AgeGroup: map[string]int{
+					"0-30":  1,
+					"31-60": 2,
+					"61+":   0,
+					"N/A":   1,
+				},
+			},
+		},
+	}
+
+	for _, v := range testcases {
+		result, err := GetSummary(v.given.([]models.CovidStat))
+		if err != nil {
+			t.Errorf("get summary error: %#v\n", err)
+		}
+		if v.want != result {
+			t.Errorf("given %#v want %#v but get %#v\n", v.given, v.want, result)
+		}
+	}
 }
